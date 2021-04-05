3a03ebef448b383c8c0034815990395e095f7602
agandybin <a.gandybin@yandex.ru>
2021-04-05 10:01:57 +0300

    3.3. Разбор первой программы. [#508368]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String [] args) {
+        System.out.println("Artem Gandybin");
+        System.out.println("12.11.1985");
+    }
+}
